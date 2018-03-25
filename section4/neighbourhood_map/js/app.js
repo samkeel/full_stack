bc143eb059b84b16caa3c565ba4f542aaa30d757
Sam Keel <programming@vectors.net.au>
2018-03-25 22:24:15 +0800

    Add files via upload

---
+++ section4/neighbourhood_map/js/app.js
@@ -0,0 +1,137 @@
+//global variables
+var map;
+var infWindow;
+var dataArray = locations;
+
+//map error catch
+var mapError = function errorAlert() {
+	alert('Unable to load Google Maps. Please try again.');
+};
+
+//populate the InfoWindow
+var popInfoWindow = function (marker, infoWindow) {
+	infoWindow.marker = marker;
+	infoWindow.setContent('');
+
+	var clientId = 'XKJDNTQ1T0I4Y0TMISGQ4WRFTAQY0OKVFG3WW0BIPQTXSKX3';
+	var client_secret = 'THVGBZPQR0NNYTMWGHOCVYZLDMKD3W5GAMWR1MAFRAKMVH2A';
+	var requestURL =
+		'https://api.foursquare.com/v2/venues/search?query="' +
+		marker.title +
+		'"&ll=' +
+		marker.position.lat() +
+		',' +
+		marker.position.lng() +
+		'&client_id=' +
+		clientId +
+		'&client_secret=' +
+		client_secret +
+		'&v=20180321';
+
+	$.getJSON(requestURL)
+		.done(function (data) {
+			var response = data.response.venues['0'];
+			// set display values
+			self.name = response.name;
+			self.address =
+				response.location.formattedAddress[0] +
+				' , ' +
+				response.location.formattedAddress[1];
+			self.here = response.hereNow.summary;
+
+			// InfoWindow output
+			self.infoContent =
+				'<div class="infowin"><h3>' +
+				self.name +
+				'</h3><string>' +
+				self.address +
+				'<br>' +
+				'There is ' +
+				self.here +
+				'</string></div>';
+			infoWindow.setContent(infoContent);
+		})
+		.fail(function () {
+			infoContent =
+				'<div class="infowin"><h3>' +
+				marker.title +
+				'</h3><string>Error occured loading the FourSquare API</string></div>';
+		});
+
+	infoWindow.open(map, marker);
+	infoWindow.addListener('closeclick', function () {
+		infoWindow.marker = null;
+	});
+};
+
+//main view model
+var ViewModel = function () {
+	var self = this;
+	this.markers = [];
+	this.searchBar = ko.observable('');
+	var centerLocation = { lat: -31.9530948, lng: 115.854724 };
+
+	//create and populate marker infoWindow's
+	this.createInfo = function () {
+		popInfoWindow(this, infWindow);
+		this.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(() => {
+			this.setAnimation(null);
+		}, 1500);
+	};
+
+	//display map
+	this.initMap = function () {
+		map = new google.maps.Map(document.getElementById('map'), {
+			styles: mapStyle,
+			zoom: 17,
+			center: new google.maps.LatLng(centerLocation),
+		});
+
+		//create InfoWindow
+		infWindow = new google.maps.InfoWindow();
+
+		//create markers
+		var marker;
+		//loop through places.js placing markers on map
+		for (var i = 0; i < dataArray.length; i++) {
+			marker = new google.maps.Marker({
+				position: new google.maps.LatLng(dataArray[i].lat, dataArray[i].lng),
+				map: map,
+				title: dataArray[i].title,
+				animation: google.maps.Animation.DROP,
+			});
+			//push marker into marker array
+			self.markers.push(marker);
+			//set marker listener for click events
+			marker.addListener('click', self.createInfo);
+		}
+
+		//close infowindow when clicking on map
+		map.addListener('click', function () {
+			infWindow.close();
+		});
+	};
+	this.initMap();
+
+	//filter list and markers
+	this.filteredItems = ko.computed(function () {
+		var filteredLocations = [];
+		for (var i = 0; i < this.markers.length; i++) {
+			var pointer = this.markers[i];
+			var query = pointer.title.toLowerCase();
+			if (query.includes(this.searchBar().toLowerCase())) {
+				filteredLocations.push(pointer);
+				this.markers[i].setVisible(true);
+			} else {
+				this.markers[i].setVisible(false);
+			}
+		}
+		return filteredLocations;
+	}, this);
+};
+
+function startApp() {
+	// Initialise the view model
+	ko.applyBindings(new ViewModel());
+}
---
+++ section4/neighbourhood_map/js/places.js
@@ -0,0 +1,38 @@
+var locations = [
+	{
+		id: 1,
+		title: 'Graffiti',
+		lat: -31.9530948,
+		lng: 115.854724,
+	},
+	{
+		id: 2,
+		title: 'DripHouse',
+		lat: -31.95326372297126,
+		lng: 115.85617627409857,
+	},
+	{
+		id: 3,
+		title: 'Louve',
+		lat: -31.955012739364953,
+		lng: 115.85329413944184,
+	},
+	{
+		id: 4,
+		title: 'Cafe Vinyl',
+		lat: -31.952229203536813,
+		lng: 115.85245173064251,
+	},
+	{
+		id: 5,
+		title: 'Jean Pierre Sancho',
+		lat: -31.95455166767558,
+		lng: 115.8558203199582,
+	},
+	{
+		id: 6,
+		title: 'The Deck Cafe Lounge',
+		lat: -31.954028782906416,
+		lng: 115.85560603899361,
+	}
+];
